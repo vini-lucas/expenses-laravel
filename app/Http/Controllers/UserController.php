54241e7fa2596ceebab0a59675cb540fd617347a
Lucas Vinicius <lucasvini269@gmail.com>
2026-02-20 17:56:18 -0300

    feat: criando tabela usuários

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::get();
+        return view('users.index', ['users' => $users]);
     }
 
     /**
